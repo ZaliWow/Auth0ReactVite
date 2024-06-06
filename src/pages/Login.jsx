960bbdef909840c330f061e711af5dc1c7963e3d
morales.william <morales.william@correounivalle.edu.co>
2024-06-06 15:10:52 +0000

    install motion

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import "../styles/login.css"
+
 import { useAuth0 } from "@auth0/auth0-react"
 import { useNavigate } from 'react-router-dom'
 import { useEffect } from "react"
@@ -18,8 +18,18 @@ export function Login({loged, setLoged}) {
       
     }, [isAuthenticated])
     return (
-        <div className="login">
-            <button onClick={() => loginWithRedirect()}>Login</button>
+        <div className="w-full h-screen justify-center items-center flex bg-HeroHome bg-center bg-cover">
+            <div className="bg-pink-50/70 rounded-lg p-4">
+                <h1 className="text-black text-center text-2xl font-bold mb-4">Bienvenido</h1>
+                <p className="text-black text-lg mb-4">Inicia sesión para acceder a tu cuenta.</p>
+                <div className="flex flex-col gap-4">
+                    <button className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded"  onClick={() => loginWithRedirect()}>Entrar</button>
+                </div>
+            </div>
+           
+           
         </div>
     )
 }
+
+  
